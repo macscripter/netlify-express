9d01a84fd79403f49e76f7f699410f20b0da3454
Alex Pavlov <macscripter@gmail.com>
2020-07-26 20:40:35 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 const router = express.Router();
 router.get("/", (req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
-  res.write("<h1>Hello from Express.js, Alexei!!!?</h1>");
+  res.write("<h1>Hello from Express.js, Alexo!!!!!</h1>");
   res.end();
 });
 router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
